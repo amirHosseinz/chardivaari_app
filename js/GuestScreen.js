6bd9679d0ed23a15e32cf6ba0f776e4eb20bd85a
Amir HS <amirhossein.zarezadeh@gmail.com>
2017-11-02 18:00:18 +0330

    add renderUtilities to add bulk utilities for mohsen

--- js/GuestScreen.js
+++ js/GuestScreen.js
@@ -120,32 +120,27 @@ class GuestScreen extends Component {
               style={{ height: 62,  elevation: 8, position: 'absolute', left: 0, bottom: 0, right: 0 }}
               innerStyle={{ paddingBottom: 0}}
               onTabChange={(newTabIndex) => this._onTabChange(newTabIndex)}
-              shifting={false}
-              >
+              shifting={false}>
                 <Tab
                   barBackgroundColor="#fff"
                   label={<Text style={styles.buttomNavFont}>حساب کاربری</Text>}
                   icon={<Icon size={24} color="#a0a0a0" name="account-circle" />}
-                  activeIcon={<Icon size={24} color="#f56e4e" name="account-circle" />}
-                />
+                  activeIcon={<Icon size={24} color="#f56e4e" name="account-circle" />} />
                 <Tab
                   barBackgroundColor="#fff"
                   label={<Text style={styles.buttomNavFont}>پیام ها</Text>}
                   icon={<Icon size={24} color="#a0a0a0" name="forum" />}
-                  activeIcon={<Icon size={24} color="#f56e4e" name="forum" />}
-                />
+                  activeIcon={<Icon size={24} color="#f56e4e" name="forum" />} />
                 <Tab
                   barBackgroundColor="#fff"
                   label={<Text style={styles.buttomNavFont}>سفرها</Text>}
                   icon={<Icon size={24} color="#a0a0a0" name="public" />}
-                  activeIcon={<Icon size={24} color="#f56e4e" name="public" />}
-                />
+                  activeIcon={<Icon size={24} color="#f56e4e" name="public" />} />
                 <Tab
                   barBackgroundColor="#fff"
                   label={<Text style={styles.buttomNavFont}>جستجو</Text>}
                   icon={<Icon size={24} color="#a0a0a0" name="search" />}
-                  activeIcon={<Icon size={24} color="#f56e4e" name="search" />}
-                />
+                  activeIcon={<Icon size={24} color="#f56e4e" name="search" />} />
         </BottomNavigation>
       </View>
     );
